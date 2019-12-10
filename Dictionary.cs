24483426e5883471f85169200358506c5082a5da
HaciIsma <haciismayilov5@outlook.com>
2019-12-10 12:58:12 -0800

    first commit

---
+++ Dictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Dictionary
+    {
+        private Pair[] pairs = null;
+        int size = default(int);
+        public string this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < pairs.Length; i++)
+                {
+                    if (pairs[i].Key == index)
+                    {
+                        return pairs[i].Value;
+                    }
+                }
+                return null;
+            }
+            set
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (pairs[i].Key == index)
+                    {
+                        pairs[i].Value = value;
+                        return;
+                    }
+                }
+                Pair[] temp = new Pair[++size];
+                if (pairs != null)
+                {
+                    Array.Copy(pairs, temp, size - 1);
+                }
+                temp[size - 1] = new Pair { Key = index, Value = value };
+                pairs = temp;
+            }
+        }
+    }
+}
---
+++ Pair.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp3
+{
+    class Pair
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
